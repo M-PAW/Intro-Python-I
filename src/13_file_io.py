6c57dd36430e5c4a7353b9ccda0150a0c6259d84
Michael Walter <michael@MacBook-Pro.local>
2020-06-01 14:30:25 -0600

    Solved file 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,8 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-with open ("/foo.txt") as f:
-    read_data = f.read()
+with open ("src/foo.txt") as rFile:
+    for line in rFile:
+        print(line)
+rFile.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -19,3 +21,6 @@ with open ("/foo.txt") as f:
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+wFile = open("src/bar.txt", "w")
+
+wFile.write(" Knock knock \n Who's there? \n Aya Wanna ")
